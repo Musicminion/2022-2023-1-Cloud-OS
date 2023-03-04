c3913321201c2456503db28c34342c59eee3531e
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-03-04 14:10:49 +0800

    完成Lab1 GBN和SR

--- Lab1/Code/rdt_receiver.cc
+++ Lab1/Code/rdt_receiver.cc
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mutex>
+#include <map>
 
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
@@ -55,9 +56,13 @@ const int Packet_HashedCheckNum_Offset = Packet_ID_Sequence_Length;
 const int Packet_PayloadSize_Offset = Packet_ID_Sequence_Length + Packet_HashedCheckNum_Length;
 const int Packet_Payload_Offset = Packet_ID_Sequence_Length + Packet_HashedCheckNum_Length + Packet_PayloadSize_Length;
 
-
 // hash func
 std::hash<std::string> hash_str_receiver;
+
+
+// buffer for sr, 做GBN的时候默认了接受窗口是1所以没有用buffer，但是SR的时候就必须要buffer了
+std::map<unsigned int, message*> rdt_receiver_buffer;
+
 
 // 已经收到的最大的包
 // 按照GBN的协议，假如收到1、2、4，在收到4的时候发的ack是2，那4我们用不用缓存呢？答案暂时不
@@ -67,6 +72,20 @@ unsigned int Already_Received_Max_ID_Sequence = 0;
 void GBN_Ack(unsigned int seqID){
     // 提示：记得回收内存！
     // 超级大坑！在哪回收内存！不是在接收ack的sender的函数回收内存，是在这里直接回收！因为Receiver_ToLowerLayer函数会做一个深度拷贝！
+    
+    struct packet* ack_resp = (struct packet*) malloc(sizeof(struct packet));
+    if(ack_resp != NULL){
+        unsigned int HashedStr = hash_str_receiver(std::to_string(seqID));
+        memcpy(ack_resp->data + ACK_ID_Sequence_Offset,&seqID, ACK_ID_Sequence_Length);
+        memcpy(ack_resp->data + ACK_HashedCheckNum_Offset, &HashedStr, ACK_HashedCheckNum_Lenght);
+        Receiver_ToLowerLayer(ack_resp);
+        
+        free(ack_resp);
+    }
+}
+
+void SR_Ack(unsigned int seqID){
+    // 提示：综合考虑，SR的ACK发送和基本没有什么特别大的差异，直接复用
     
     struct packet* ack_resp = (struct packet*) malloc(sizeof(struct packet));
     if(ack_resp != NULL){
@@ -139,7 +158,6 @@ void Receiver_FromLowerLayer_GBN(struct packet *pkt){
     memcpy(&Part_PayloadLength, pkt -> data + Packet_PayloadSize_Offset, Packet_PayloadSize_Length);
 
     
-    
     if((unsigned int)Part_PayloadLength <= Packet_Payload_Length){
         std::string dataStr(pkt -> data + Packet_Payload_Offset, pkt -> data + Packet_Payload_Offset + Part_PayloadLength);
         //printf("%u !\n", Part_PayloadLength);
@@ -148,7 +166,6 @@ void Receiver_FromLowerLayer_GBN(struct packet *pkt){
         // printf("[Receiver_FromLowerLayer_GBN] point1 \n");
         // 消息没有被篡改 
         if(hash_cal == Part_Hashed){
-            
             // 乱序了！这时候处理方法，send 之前已经顺序收到的最大的ACK，
             // 举例：依次收到1、2、3、6，处理6的收到的时候，发的ack是ack3！这个包裹也不往上传输，等下一回重发
             if(Part_SeqID != Already_Received_Max_ID_Sequence + 1){
@@ -171,7 +188,6 @@ void Receiver_FromLowerLayer_GBN(struct packet *pkt){
 
                 Receiver_ToUpperLayer(rest_msg);
                 GBN_Ack(Part_SeqID);
-
                 
                 if(rest_msg ->data != NULL) free(rest_msg ->data);
                 if(rest_msg != NULL) free(rest_msg);
@@ -186,12 +202,110 @@ void Receiver_FromLowerLayer_GBN(struct packet *pkt){
     rdt_receiver_mutex.unlock();
 }
 
+// 接受者处理SR的逻辑
+void Receiver_FromLowerLayer_SR(struct packet *pkt){
+    ASSERT(pkt != NULL)
+    rdt_receiver_mutex.lock();
+
+    // 获取包里面的相关值
+    unsigned int Part_SeqID = 0;
+    memcpy(&Part_SeqID, pkt -> data + Packet_ID_Sequence_Offset, Packet_ID_Sequence_Length);
+    
+    // 把Hash Value拷贝过去
+    unsigned int Part_Hashed = 0;
+    memcpy(&Part_Hashed, pkt -> data + Packet_HashedCheckNum_Offset, Packet_HashedCheckNum_Length);
+    
+    // 把Payload Length拷贝过去
+    unsigned char Part_PayloadLength = 0;
+    memcpy(&Part_PayloadLength, pkt -> data + Packet_PayloadSize_Offset, Packet_PayloadSize_Length);
+
+    
+    if((unsigned int)Part_PayloadLength <= Packet_Payload_Length){
+        std::string dataStr(pkt -> data + Packet_Payload_Offset, pkt -> data + Packet_Payload_Offset + Part_PayloadLength);
+        //printf("%u !\n", Part_PayloadLength);
+        // 开始校验是否信息被篡改过！
+        unsigned int hash_cal = hash_str_receiver(std::to_string(Part_SeqID) + std::to_string(Part_PayloadLength) + dataStr);
+        // printf("[Receiver_FromLowerLayer_GBN] point1 \n");
+        // 消息没有被篡改 
+        // 
+        if(hash_cal == Part_Hashed){
+            // if(Part_SeqID <= Already_Received_Max_ID_Sequence){
+            //     SR_Ack(Part_SeqID);
+            // }
+            // 乱序了！这时候处理方法，SR会先缓存所有数据，发送对应的ACK
+            // 举例：依次收到1、2、3、6，处理6的收到的时候，发ack6，然后等待4、5的发来或者超时重新传输
+            if(Part_SeqID != Already_Received_Max_ID_Sequence + 1){
+                // 定义一个rest msg
+                struct message *rest_msg = (struct message*) malloc(sizeof(struct message));
+                ASSERT(rest_msg != NULL)
+                // 把payload 拷贝过去
+                rest_msg -> size = (int)Part_PayloadLength;
+                rest_msg -> data = (char*)malloc(rest_msg ->size);
+                ASSERT(rest_msg -> data != NULL);
+                memcpy(rest_msg -> data, pkt -> data + Packet_Payload_Offset,  Part_PayloadLength);
+
+                rdt_receiver_buffer[Part_SeqID] = rest_msg;
+                SR_Ack(Part_SeqID);
+                //printf("收到！ expect: %u [乱序]%u \n", Already_Received_Max_ID_Sequence + 1 , Part_SeqID);
+            }
+            // 是下一个要接受的包，没有乱序
+            else{
+
+                Already_Received_Max_ID_Sequence = Part_SeqID;
+                // 定义一个rest msg
+                struct message *rest_msg = (struct message*) malloc(sizeof(struct message));
+                ASSERT(rest_msg != NULL)
+                
+                // 把payload 拷贝过去
+                
+                rest_msg -> size = (int)Part_PayloadLength;
+                rest_msg -> data = (char*)malloc(rest_msg ->size);
+
+                ASSERT(rest_msg -> data != NULL);
+                memcpy(rest_msg -> data, pkt -> data + Packet_Payload_Offset,  Part_PayloadLength);
+
+                Receiver_ToUpperLayer(rest_msg);
+                //printf("收到！[顺序]%u \n", Part_SeqID);
+                SR_Ack(Part_SeqID);
+ 
+                if(rest_msg ->data != NULL) free(rest_msg ->data);
+                if(rest_msg != NULL) free(rest_msg);
+
+                //此外还要加一个步骤，检查buffer里面，如果buffer不是空，且正好是刚接手的下一个，那我就要处理buffer里面的数据
+                //printf("size %u \n" , rdt_receiver_buffer.size());
+
+                while(!rdt_receiver_buffer.empty() && rdt_receiver_buffer.begin()->first <= Already_Received_Max_ID_Sequence){
+                    rdt_receiver_buffer.erase(rdt_receiver_buffer.begin());
+                }
+                
+                while(!rdt_receiver_buffer.empty() && rdt_receiver_buffer.begin()->first == Already_Received_Max_ID_Sequence + 1){
+                    //printf("收到！[while]%u \n", rdt_receiver_buffer.begin()->first);
+                    struct message *rest_msg = rdt_receiver_buffer.begin()->second;
+                    Receiver_ToUpperLayer(rdt_receiver_buffer.begin()->second);
+                    rdt_receiver_buffer.erase(rdt_receiver_buffer.begin());
+                    Already_Received_Max_ID_Sequence += 1;
+
+                    // 回收内存！
+                    if(rest_msg ->data != NULL) free(rest_msg ->data);
+                    if(rest_msg != NULL) free(rest_msg);
+                }
+            }
+        }
+    }
+
+    // memcpy(pkt -> data + Packet_Payload_Offset, msg -> data + cursor, Part_PayloadLength);
+    // printf("[Receiver_FromLowerLayer_GBN] end \n");
+    rdt_receiver_mutex.unlock();
+}
+
+
 
 /* event handler, called when a packet is passed from the lower layer at the 
    receiver */
 void Receiver_FromLowerLayer(struct packet *pkt)
 {
     Receiver_FromLowerLayer_GBN(pkt);
+    // Receiver_FromLowerLayer_SR(pkt);
 }
 
 
--- Lab1/Code/rdt_sender.cc
+++ Lab1/Code/rdt_sender.cc
@@ -21,7 +21,6 @@
 // 5. 测试找bug！
 // 6. 同样的方法 写SR选择回传
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,7 +28,6 @@
 #include <deque>
 #include <map>
 #include <string>
-
 
 #include "rdt_struct.h"
 #include "rdt_sender.h"
@@ -78,7 +76,6 @@ const int ACK_HashedCheckNum_Offset = ACK_ID_Sequence_Length;
 
 
 
-
 // for set timer, according to lab requirement, 0.3 is suggested
 const double TimeOutValue = 0.3;
 
@@ -97,6 +94,8 @@ unsigned int Already_Send_Max_ID_Sequence = 0;
 // hash func
 std::hash<std::string> hash_str_sender;
 
+// send if start
+bool send_start = false;
 
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
@@ -107,6 +106,7 @@ void Sender_Init()
     // init move window left point to 0, right to window size - 1
     Moving_Window_Left_Index = 0;
     Moving_Window_Right_Index = Moving_Window_Size - 1;
+    send_start = false;
 }
 
 /* sender finalization, called once at the very end.
@@ -119,6 +119,7 @@ void Sender_Final()
     rdt_sender_buffer.clear();
     Moving_Window_Left_Index = 0;
     Moving_Window_Right_Index = Moving_Window_Size - 1;
+    send_start = false;
 }
 
 // 这个是原版提供，为了展示对比效果，我单独把原始的代码部分抽象提出来了
@@ -199,15 +200,17 @@ void Sender_FromUpperLayer_GBN(struct message *msg){
         
         rdt_sender_buffer.push_back(pkt);
     }
-    
-    // GBN 发前面的N个
-    for(unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index && i <rdt_sender_buffer.size(); i++){
-        Sender_ToLowerLayer(&rdt_sender_buffer[i]);
-        // buffer 里面的 buffer[i] 对应seqID i+1
-        Already_Send_Max_ID_Sequence = std::max(Already_Send_Max_ID_Sequence, i + 1);
+
+    if(!send_start){
+        // GBN 发前面的N个
+        for(unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index && i <rdt_sender_buffer.size(); i++){
+            Sender_ToLowerLayer(&rdt_sender_buffer[i]);
+            // buffer 里面的 buffer[i] 对应seqID i+1
+            Already_Send_Max_ID_Sequence = std::max(Already_Send_Max_ID_Sequence, i + 1);
+        }
+        Sender_StartTimer(TimeOutValue);
+        send_start = true;
     }
-    
-    Sender_StartTimer(TimeOutValue);
     rdt_sender_mutex.unlock();
 }
 
@@ -258,11 +261,6 @@ void Sender_FromLowerLayer_GBN(struct packet *pkt)
 
 
 
-
-
-
-
-
 void Sender_Timeout_GBN()
 {
     rdt_sender_mutex.lock();
@@ -284,6 +282,138 @@ void Sender_Timeout_GBN()
 }
 
 
+
+void Sender_FromUpperLayer_SR(struct message *msg){
+    rdt_sender_mutex.lock();
+    
+    packet pkt;
+    for(int cursor = 0; cursor < msg ->size; cursor += Packet_Payload_Length){
+        /* take msg into many Parts */
+        /* Calculate currently Packet Seq ID and this Part Payload Length */
+        /* Start from 1, to ++.. */
+        
+        unsigned int Part_SeqID = rdt_sender_buffer.size() + 1;
+
+        unsigned char Part_PayloadLength = std::min(msg -> size - cursor, Packet_Payload_Length);
+        /* convert data to string in order to joint */
+        std::string dataStr(msg -> data + cursor, msg -> data + cursor +  Part_PayloadLength);
+        /* get hashed val */
+        unsigned int Part_Hashed = hash_str_sender(std::to_string(Part_SeqID) + std::to_string(Part_PayloadLength) + dataStr);
+        
+        /* fill in the packet */
+        // 把SeqID拷贝过去
+        memcpy(pkt.data + Packet_ID_Sequence_Offset , &Part_SeqID, Packet_ID_Sequence_Length);
+        // 把Hash Value拷贝过去
+        memcpy(pkt.data + Packet_HashedCheckNum_Offset, &Part_Hashed, Packet_HashedCheckNum_Length);
+        // 把payload size 拷贝过去
+        memcpy(pkt.data + Packet_PayloadSize_Offset, &Part_PayloadLength, Packet_PayloadSize_Length);
+        // 把payload 拷贝过去
+        memcpy(pkt.data + Packet_Payload_Offset, msg -> data + cursor, Part_PayloadLength);
+        
+        
+        rdt_sender_buffer.push_back(pkt);
+    }
+    
+    if(!send_start){
+        // SR 发前面的N个
+        for(unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index && i <rdt_sender_buffer.size(); i++){
+            Sender_ToLowerLayer(&rdt_sender_buffer[i]);
+            // buffer 里面的 buffer[i] 对应seqID i+1
+            printf("[init] 发送！%u \n", i + 1);
+            Already_Send_Max_ID_Sequence = std::max(Already_Send_Max_ID_Sequence, i + 1);
+        }
+        send_start = true;
+        Sender_StartTimer(TimeOutValue);
+    }
+    rdt_sender_mutex.unlock();
+}
+
+
+void Sender_FromLowerLayer_SR(struct packet *pkt){
+    ASSERT(pkt != NULL)
+
+    rdt_sender_mutex.lock();
+    // 拷贝
+    unsigned int Part_SeqID;
+    memcpy(&Part_SeqID, pkt ->data + ACK_ID_Sequence_Offset, ACK_ID_Sequence_Length);
+    unsigned int Hashed_Val;
+    memcpy(&Hashed_Val, pkt -> data + ACK_HashedCheckNum_Offset, ACK_HashedCheckNum_Lenght);
+
+    unsigned int hash_cal = hash_str_sender(std::to_string(Part_SeqID));
+
+    // 数据包不存在被篡改, 考虑到SR的确认特性，需要打上标记，说明已经收到
+    if(hash_cal == Hashed_Val && Part_SeqID > Moving_Window_Left_Index){
+        rdt_sender_ackRecord[Part_SeqID] = true;
+        
+        // 举个例子 window大小10， Moving_Window_Left_Index = 0, Moving_Window_Right_Index = 9
+        // 对应的Packet ID = 1 10
+        for (unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index; i++)
+        {
+            unsigned int check_seq_ID = i + 1;
+            
+            if(!rdt_sender_ackRecord.count(check_seq_ID)){
+            
+                //printf("mov windows fail for %u\n", check_seq_ID);
+                rdt_sender_mutex.unlock();
+                return;
+            }
+        }
+        
+        // 移动窗口！因为编号为 Part_SeqID 已经被收到了，那么就把窗口的起点往后移动
+        // 超级BUG！这里我改了半天才发现！Part_SeqID我是从1开始算的，对应的window的0
+
+        Moving_Window_Left_Index = Moving_Window_Right_Index + 1;
+        Moving_Window_Right_Index = Moving_Window_Left_Index + Moving_Window_Size - 1;
+
+        //printf("index [%u, %u ]\n", Moving_Window_Left_Index, Moving_Window_Right_Index);
+        for(unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index; i++){
+            // 变量 i 非法，不访问直接终止循环    
+            if(i >= rdt_sender_buffer.size())
+                break;
+            // 如果这个时候 i == Moving_Window_Left_Index， 说明马上要开始发包了
+            // 计时器只需要开启一次，因为是要把整个窗口里面的东西重发一遍
+            if(i == Moving_Window_Left_Index)
+                Sender_StartTimer(TimeOutValue);
+            //printf("[mov window] 发送！%u \n", i + 1);
+            Sender_ToLowerLayer(&rdt_sender_buffer[i]);
+            Already_Send_Max_ID_Sequence = std::max(Already_Send_Max_ID_Sequence, (unsigned int)(i + 1));
+        }
+        
+    }
+
+    // 注意 这里不需要回收内存，因为检查源代码，上层调用这个函数之后，会主动回收内存！
+    rdt_sender_mutex.unlock();
+}
+
+
+void Sender_Timeout_SR(){
+    rdt_sender_mutex.lock();
+    bool ifLaunched = false;
+    // 根据SR规则，遇到超时的时候选择性传没有收到的
+    for(unsigned int i = Moving_Window_Left_Index; i <= Moving_Window_Right_Index; i++){
+        // 变量 i 非法，不访问直接终止循环
+        if(i >= rdt_sender_buffer.size())
+            break;
+        // 特别注意：发起计时器的时候要谨慎！不然会死循环。        
+        if(rdt_sender_ackRecord.count(i + 1))
+            continue;
+        else{
+            //printf("超时发送！%u \n", i + 1);
+            Sender_ToLowerLayer(&rdt_sender_buffer[i]);
+            if(!ifLaunched){
+                Sender_StartTimer(TimeOutValue);
+                ifLaunched = true;
+            }
+                
+        }
+        Already_Send_Max_ID_Sequence = std::max(Already_Send_Max_ID_Sequence, (i + 1));
+    }
+
+    rdt_sender_mutex.unlock();
+
+}
+
+
 /* 按照作业要求，我在要求完成的函数上面又一次封装了一层，为了方便的展示GBN和SR两个协议的差别 */
 /* 带有GBN的函数是GBN相关的协议的处理函数 */
 
@@ -292,23 +422,23 @@ void Sender_Timeout_GBN()
 void Sender_FromLowerLayer(struct packet *pkt)
 {
     Sender_FromLowerLayer_GBN(pkt);
+    // Sender_FromLowerLayer_SR(pkt);
 }
-
-
 
 
 /* event handler, called when the timer expires */
 void Sender_Timeout()
 {
     Sender_Timeout_GBN();
+    // Sender_Timeout_SR();
 }
 
 
 /* event handler, called when a message is passed from the upper layer at the 
    sender */
-
 void Sender_FromUpperLayer(struct message *msg)
 {
+    // 测试提示：打开注释，即可测试GBN的效果
     // 特别注意！这个函数非常坑！所以一定看上层调用这个函数的逻辑，我也是找了好久才发现自己的bug
     // 我最开始以为上层会产生一个非常长的message，然后我自己切分，结果是发现上层会产生非常多的message
     // 上层函数会随机生成message，message的大小，有时候大，有时候小，大的有时候可能2-3个包裹才能装下
@@ -316,5 +446,8 @@ void Sender_FromUpperLayer(struct message *msg)
     // 如果message比较小，那就直接发出去，一个包裹
     // 因为主函数产生的速度非常快，假如第一次产生的message比较小（分成一个packet），第二次产生的大（需要两个packet）
     // 需要正确的给这些packet标号！第一次产生的message比较小作为一个标号为1的packet，第二次的作为标号为2、3的packet！
+
     Sender_FromUpperLayer_GBN(msg);
+
+    // Sender_FromUpperLayer_SR(msg);
 }
